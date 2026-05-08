24b9a43755f355436f6541e1c08010e9803b77e9
Gustavo Guimarães <gustavodossantosguimaraes93@gmail.com>
2026-05-07 21:49:02 -0300

    alteracao

--- src/main/java/com/example/security/SecurityApplication.java
+++ src/main/java/com/example/security/SecurityApplication.java
@@ -2,8 +2,11 @@ package com.example.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication
+@Configuration
+@EnableWebSecurity
 public class SecurityApplication {
 
 	public static void main(String[] args) {
